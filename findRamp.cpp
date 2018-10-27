d1215e9877062a4f14fb57e81097ecb1beace14a
greg <greg@hedgersoftware.com>
2018-10-27 14:38:40 -0600

    Added refinements/simplifications to the algorithm after some research.

--- findRamp.cpp
+++ findRamp.cpp
@@ -2,7 +2,10 @@
 // at an arbitrary point in an array and finds the start using
 // a binary search with goal of O ( log n ) performance.
 //
-// The array is sorted in ascending order. 
+// This implementation is in C to avoid the semantic clutter of C++
+// and help with the understanding of the algorithm.
+//
+// The array is sorted in ascending order.
 // Duplicate values may be present.
 // The upper value bound for the array is unknown.
 
@@ -23,12 +26,13 @@
 #include <assert.h>
 #include <vector>
 
+using namespace std;
+
 // Definitions
 typedef unsigned short CONTAINER;
 typedef __int32_t SIZE;
 typedef __uint32_t UINT;
 typedef __int32_t INT;
-#define EL std::endl
 
 // Constants
 const unsigned CONTAINER_SIZE = 32;
@@ -37,7 +41,9 @@ const unsigned RANGE = CONTAINER_SIZE;
 const unsigned MAX_DEPTH = 1 << 24;
 const unsigned INCREMENT_BOUND = 4;
 
-// @freeContainer
+// freeContainer
+// Deallocate container resources
+// Entry: pointer to container
 void freeContainer( CONTAINER *container )
 {
   if( container ) {
@@ -45,9 +51,20 @@ void freeContainer( CONTAINER *container )
   }
 }
 
+// allocContainer
+// Allocate container resources
+// Entry: size of container
+// Exit: pointer to container
 CONTAINER *allocContainer( SIZE size )
 {
   return new CONTAINER[ size ];
+}
+
+void printContainer( CONTAINER *container, SIZE size )
+{
+  for( auto i = 0; i < size; i++ )
+    cout << container[ i ] << " ";
+  cout << endl;
 }
 
 void generateRamp( CONTAINER *container, SIZE size, UINT startIdx, bool dupes )
@@ -56,7 +73,7 @@ void generateRamp( CONTAINER *container, SIZE size, UINT startIdx, bool dupes )
   CONTAINER j = 0;
   do {
     container[ i ] = j;
-    j += 1; // rand() % INCREMENT_BOUND;
+    j += dupes ? rand() % INCREMENT_BOUND : 1;
     if( !dupes ) {
       if( !j ) {
         j++;
@@ -64,106 +81,87 @@ void generateRamp( CONTAINER *container, SIZE size, UINT startIdx, bool dupes )
     }
     i = (i + 1) % size;
   } while( i != startIdx % size );
+
+  printContainer( container, size );
 }
 
-UINT findRecurse(
-    CONTAINER *container,
-    SIZE subsectionSize,
-    SIZE fullSize,
-    UINT idx,
-    UINT depth,
-    UINT *tries,
-    UINT *lwm,
-    UINT *lwmIdx
-    )
+// findRampPivot
+// Find the beginning of the ramp, or "pivot" within a rotated sorted table
+// Takes the high and low indexes, calculates a midpoint, and recurses into itself
+// zeroing in on the target.
+// Entry: pointer to container
+//        low index
+//        high index
+//        pointer to tries (for complexity analyis)
+UINT findRampPivot(
+  const CONTAINER *container,
+  UINT lowIdx,
+  UINT highIdx,
+  UINT *tries )
 {
   (*tries)++;
-  // santiy check
-  assert( depth < MAX_DEPTH );
+  // sanity check
+  if( highIdx == lowIdx )
+    return lowIdx;
+  if( highIdx < lowIdx )
+    return ~0;
 
-  UINT valL = 0, valR = 0;
-  UINT idxL, idxR, startIdx;
-  idxL = (idx - ( subsectionSize + 1 ) / 2) % fullSize;
-  idxR = (idx + ( subsectionSize + 1 ) / 2) % fullSize;
-
-  // skip past any dupes, guarding against infinite loop
-  valL = container[ idxL ];
-  valR = container[ idxR ];
-  startIdx = valL;
-  while( valL == valR && valL != startIdx ) {
-    idxL = ( idxL + 1 ) % fullSize;
-    idxR = ( idxR + 1 ) % fullSize;
-    idxL = container[ idxL ];
-    valR = container[ idxR ];
+  // Zero in on the pivot point based on the relative quantities
+  // at the different indexes.
+  UINT midIdx = ( lowIdx + highIdx ) >> 1;
+  if( midIdx < highIdx && container[ midIdx ] > container[ midIdx + 1 ] )
+    return midIdx;
+  if( midIdx > lowIdx && container[ midIdx ] < container[ midIdx - 1 ] )
+    return midIdx - 1;
+  if( container[ lowIdx ] >= container[ midIdx ] ) {
+    return findRampPivot( container, lowIdx, midIdx - 1, tries );
   }
-
-  // Update low water mark
-  if( valL < *lwm ) {
-    *lwm = valL;
-    *lwmIdx = idxL;
-  }
-  if( valR < *lwm ) {
-    *lwm = valR;
-    *lwmIdx = idxR;
-  }
-  if( idx < *lwm ) {
-    *lwm = container[ idx ];
-    *lwmIdx = idx;
-  }
-
-  if( subsectionSize ) {
-    return findRecurse(
-        container,
-        subsectionSize >> 1,
-        fullSize,
-        *lwmIdx,
-        ++depth,
-        tries,
-        lwm,
-        lwmIdx
-        );
-  } else {
-    return *lwmIdx;
-  }
+  return findRampPivot( container, midIdx + 1, highIdx, tries );
 }
 
+// findRampStart
 // Find the transition between 0 and n (ramp start)
+// Entry: pointer to container
+//        size of container in elements
+//        pointer to tries count (for complexity analysis)
 UINT findRampStart(
     CONTAINER *container,
     SIZE size,
-    UINT startIdx,
     UINT *tries
     )
 {
-  int i = startIdx;
-  UINT lwm = ~0;
-  UINT lwmIdx = 0;
-  return findRecurse(
-      container,
-      size / 2,
-      size,
-      startIdx,
-      0,
-      tries,
-      &lwm,
-      &lwmIdx
-      );
+  UINT pivot;
+
+  // First, check for edge case where the pivot seam matches the bounds of the array
+  // (i.e. array is not rotated)
+  if( !container[ 0 ] )
+    return 0;
+  pivot = findRampPivot( container, 0, size - 1, tries );
+
+  // EDGE CASE: Skip any repeated entries
+  while( container[ ( pivot + 1 ) % size ] == container[ pivot ] )
+    pivot = ( pivot + 1 ) % size;
+
+  if( ~0 != pivot )
+    pivot = ( pivot + 1 ) % size;
+  return pivot;
 }
 
 void printUsage()
 {
-  std::cout << "FindRamp" << EL;
-  std::cout << "Copyright (C) 2018 Gregory Hedger" << EL;
-  std::cout << "Usage:" << EL;
-  std::cout << "\tfindramp <container_size> <#_of_iterations> <duplications>" << EL;
-  std::cout << "Example:" << EL;
-  std::cout << "\tfindramp 250 10000 1" << EL;
+  cout << "FindRamp" << endl;
+  cout << "Copyright (C) 2018 Gregory Hedger" << endl;
+  cout << "Usage:" << endl;
+  cout << "\tfindramp <container_size> <#_of_iterations> <duplications>" << endl;
+  cout << "Example:" << endl;
+  cout << "\tfindramp 250 10000 1" << endl;
 }
 
 int main( int argc, char *argv[])
 {
   // Seed prandom with time and get startIdx
-  srand( ( UINT ) time( NULL ) );
+  //srand( ( UINT ) time( NULL ) );
+  srand( 53 );
 
   // grab params
   UINT containerSize;
@@ -188,6 +186,10 @@ int main( int argc, char *argv[])
 
   // Allocate and generate container
   CONTAINER *container = allocContainer( containerSize );
+  if( !container ) {
+    cout << "Memory allocation error" << endl;
+    return -1;
+  }
 
   // Perform test
   double sigma = 0.0, mu = 0.0;
@@ -195,14 +197,29 @@ int main( int argc, char *argv[])
   std::vector<UINT> triesVect;
   for( UINT i = 0; i < iterationTot; i++ )
   {
+    //srand( i );
     UINT startIdx = rand() % containerSize;
     generateRamp( container, containerSize, startIdx, allowDuplicates );
     UINT tries = 0;
     UINT idx = findRampStart(
         container,
         containerSize,
-        containerSize >> 1,
         &tries );
+    if( ~0 == idx ) {
+      cout << "Error in search parameters." << endl;
+    }
+    // In this test, it should always find 0.
+    // If it does not, that is noteworthy and indicates a bug
+    else if( container[ idx ] ) {
+      cout << "Error finding element. idx 0:" << container[0] << endl;
+      cout << "Reported: " << idx << ":" << container[idx] << "  ";
+
+   }
+   if( ~0 == idx || container[ idx ] ) {
+       cout << "TEST " << i << ": Actual: " << ( containerSize - container[0] ) % containerSize << ":" <<
+        container[ containerSize - container[0] ] << endl;
+   }
+
     triesAccum += tries;
     triesVect.push_back( tries );
   }
@@ -219,8 +236,8 @@ int main( int argc, char *argv[])
   }
   sigma = sqrt( sigmaAccum / iterationTot );
 
-  std::cout << "TRIES MU: " << mu << EL;
-  std::cout << "TRIES SIGMA: " << sigma << EL;
+  cout << "TRIES MU: " << mu << endl;
+  cout << "TRIES SIGMA: " << sigma << endl;
 
   // Clean up and go home
   freeContainer( container );
